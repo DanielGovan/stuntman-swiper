be37583beed7d86b9115d62fac53f6e0abf2d3c4
Dan Govan <daniel.govan@gmail.com>
2020-07-26 13:42:56 +0100

    Stack reducing

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,28 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./Stack.module.css";
 import SwipeCard from "./SwipeCard";
 
 function Stack({ data }) {
-  const [storedData, setStoredData] = useState(data);
+  const [currentCards, setCurrentCards] = useState(data);
+  const [chosen, setChosen] = useState([]);
   const [dragging, setDragging] = useState(false);
-  // changes styles when dragging to make it look like the card is moving
+  const [pickUp, setPickUp] = useState(0);
+
+  const dragCard = useRef();
 
   const handleDragStart = (e) => {
-    console.log("Drag started");
-    e.target.addEventListener("dragend", handleDragEnd);
+    dragCard.current = e.target;
+    dragCard.current.addEventListener("dragend", handleDragEnd);
+    setPickUp(e.clientX);
     setTimeout(() => {
+      // changes styles when dragging to make it look like the card is moving
       setDragging(true);
     }, 0);
   };
 
   const handleDragEnd = (e) => {
-    console.log("Drag ended");
+    if (pickUp > e.clientX) {
+      handleNextCard("no");
+    } else if (pickUp < e.clientX) {
+      handleNextCard("yes");
+    }
+    //resets listener and ref and state
+    dragCard.current.removeEventListener("dragend", handleDragEnd);
+    dragCard.current = null;
     setDragging(false);
+  };
+
+  const handleNextCard = (pick) => {
+    if (pick == "yes") {
+      console.log("keep");
+      //setChosen([...chosen.push(currentCards[0])]);
+      //Push to kept
+    } else {
+      console.log("discard");
+    }
+    setCurrentCards([...currentCards.slice(1)]); //removes the top card
+    //console.log(chosen, currentCards[0]);
   };
 
   return (
     <div className={styles.stack}>
-      {storedData
+      {currentCards
         .map((item) => (
           <SwipeCard
             draggable
